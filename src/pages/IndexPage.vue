037cc6df305267c7bbe9f72234029413778e74e1
Hlib <hlib.monastyrov@gmail.com>
2024-02-10 21:43:02 +0200

    Prepared text for cities

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,32 +1,52 @@
 <template>
-  <div class="q-pa-md" style="max-width: 350px">
-    <q-expansion-item
-      class="shadow-1 overflow-hidden"
-      style="border-radius: 30px"
-      icon="explore"
-      label="Counter"
-      @show="startCounting"
-      @hide="stopCounting"
-      header-class="bg-primary text-white"
-      expand-icon-class="text-white"
-    >
-      <q-card>
-        <q-card-section>
-          Counting: <q-badge color="secondary">{{ counter }}</q-badge
-          >. Will only count when opened, using the show/hide events to control
-          count timer.
-        </q-card-section>
-      </q-card>
-    </q-expansion-item>
-  </div>
+  <div style="display: flex; justify-content: center">
+    <div class="q-pa-md" style="max-width: 350px">
+      <q-expansion-item
+        class="shadow-1 overflow-hidden"
+        style="border-radius: 30px"
+        icon="explore"
+        label="About # 1"
+        header-class="bg-primary text-white"
+        expand-icon-class="text-white"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore a
+            dolores sequi inventore recusandae placeat temporibus minima vitae
+            beatae modi animi fuga aperiam quasi, nobis tempora quae
+            necessitatibus enim ipsa?
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </div>
 
+    <div class="q-pa-md" style="max-width: 350px">
+      <q-expansion-item
+        class="shadow-1 overflow-hidden"
+        style="border-radius: 30px"
+        icon="explore"
+        label="About # 2"
+        header-class="bg-primary text-white"
+        expand-icon-class="text-white"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore a
+            dolores sequi inventore recusandae placeat temporibus minima vitae
+            beatae modi animi fuga aperiam quasi, nobis tempora quae
+            necessitatibus enim ipsa?
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </div>
+  </div>
   <div>
     <q-splitter v-model="splitterModel" style="height: 250px">
       <template v-slot:before>
         <q-tabs v-model="tab" vertical class="text-teal">
-          <q-tab name="mails" icon="mail" label="Mails" />
-          <q-tab name="alarms" icon="alarm" label="Alarms" />
-          <q-tab name="movies" icon="movie" label="Movies" />
+          <q-tab name="Kyiv" icon="map" label="Kyiv" />
+          <q-tab name="London" icon="map" label="London" />
+          <q-tab name="Paris" icon="map" label="Paris" />
         </q-tabs>
       </template>
 
@@ -39,58 +59,19 @@
           transition-prev="jump-up"
           transition-next="jump-up"
         >
-          <q-tab-panel name="mails">
-            <div class="text-h4 q-mb-md">Mails</div>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
+          <q-tab-panel name="Kyiv">
+            <div class="text-h4 q-mb-md">Weather in Kyiv</div>
+            <p>Weather in Kyiv</p>
           </q-tab-panel>
 
-          <q-tab-panel name="alarms">
-            <div class="text-h4 q-mb-md">Alarms</div>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
+          <q-tab-panel name="London">
+            <div class="text-h4 q-mb-md">Weather in London</div>
+            <p>Weather in London</p>
           </q-tab-panel>
 
-          <q-tab-panel name="movies">
-            <div class="text-h4 q-mb-md">Movies</div>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam
-              possimus obcaecati commodi minima assumenda consectetur culpa fuga
-              nulla ullam. In, libero.
-            </p>
+          <q-tab-panel name="Paris">
+            <div class="text-h4 q-mb-md">Weather in Paris</div>
+            <p>Weather in Paris</p>
           </q-tab-panel>
         </q-tab-panels>
       </template>
@@ -102,50 +83,16 @@
       <q-timeline-entry heading body="What is this website?" />
 
       <q-timeline-entry
-        title="Cross-platform programming"
-        subtitle="What is so special about Quasar?"
+        title="OpenWeather.com"
+        subtitle="How do we access weather data?"
         avatar="~assets/Avatar.jpg"
-        :body="cross_platform_text"
+        :body="par1"
       />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 21, 1986"
-        icon="delete"
-        :body="body"
-      />
-
-      <q-timeline-entry heading body="November, 2017" />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
       <q-timeline-entry
         title="Event Title"
         subtitle="February 22, 1986"
         color="orange"
         icon="done_all"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        :body="body"
-      />
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
         :body="body"
       />
     </q-timeline>
@@ -154,34 +101,15 @@
 
 <script>
 import { ref } from "vue";
-import { onBeforeUnmount } from "vue";
 
 export default {
   setup() {
-    const counter = ref(0);
-    let timer;
-
-    function stopCounting() {
-      clearInterval(timer);
-    }
-
-    onBeforeUnmount(stopCounting);
-
     return {
-      cross_platform_text: "This is text",
+      par1: "We use OpenWeather.com",
+      par2: "",
       body: "Lorem ipsum",
       tab: ref("mails"),
       splitterModel: ref(20),
-
-      counter,
-
-      startCounting() {
-        timer = setInterval(() => {
-          counter.value++;
-        }, 1000);
-      },
-
-      stopCounting,
     };
   },
 };
