6275085cbcb728a1418952cc185b2ccb67a3accc
Hlib <hlib.monastyrov@gmail.com>
2024-02-11 11:04:42 +0200

    layout and functions update

--- src/pages/WeatherPanel.vue
+++ src/pages/WeatherPanel.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
   <div class="q-pa-md q-gutter-sm">
     <q-banner rounded :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-2'">
       <template v-slot:avatar>
@@ -12,103 +12,63 @@
     </q-banner>
   </div>
 
-  <div class="q-pa-md q-gutter-md">
-    <q-btn color="primary" @click="showTextLoading"> Show it </q-btn>
+  <div style="display: flex; justify-content: center">
+    <div class="q-pa-md q-gutter-md">
+      <q-card class="relative-position card-example" flat bordered>
+        <q-card-section class="q-pb-none">
+          <div class="text-h6">Weather</div>
+        </q-card-section>
 
-    <q-card class="relative-position card-example" flat bordered>
-      <q-card-section class="q-pb-none">
-        <div class="text-h6">Lorem Ipsum</div>
-      </q-card-section>
+        <q-card-section>
+          <transition
+            appear
+            enter-active-class="animated fadeIn"
+            leave-active-class="animated fadeOut"
+          >
+            <div v-show="showSimulatedReturnData">
+              {{ textFieldValue }}
+            </div>
+          </transition>
+        </q-card-section>
 
-      <q-card-section>
-        <transition
-          appear
-          enter-active-class="animated fadeIn"
-          leave-active-class="animated fadeOut"
-        >
-          <div v-show="showSimulatedReturnData">Lorem ipsum dolor sit amet</div>
-        </transition>
-      </q-card-section>
+        <q-inner-loading :showing="visible">
+          <q-spinner-gears size="50px" color="primary" />
+        </q-inner-loading>
+      </q-card>
+    </div>
 
-      <q-inner-loading :showing="visible">
-        <q-spinner-gears size="50px" color="primary" />
-      </q-inner-loading>
-    </q-card>
-  </div>
-</template>
-
-<script>
-import { ref } from "vue";
-
-// Don't forget to specify which animations
-// you are using in quasar.config file > animations.
-// Alternatively, if using UMD, load animate.css from CDN.
-export default {
-  setup() {
-    const visible = ref(false);
-    const showSimulatedReturnData = ref(false);
-
-    return {
-      visible,
-      showSimulatedReturnData,
-
-      showTextLoading() {
-        visible.value = true;
-        showSimulatedReturnData.value = false;
-
-        setTimeout(() => {
-          visible.value = false;
-          showSimulatedReturnData.value = true;
-        }, 3000);
-      },
-    };
-  },
-};
-</script>
-
-<style lang="sass" scoped>
-.card-example
-  width: 288px
-  height: 290px
-</style> -->
-
-<template>
-  <div class="q-pa-md" style="max-width: 400px">
-    <q-form @submit="onSubmit" @reset="loadData" class="q-gutter-md">
-      <q-input
-        filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
-      />
-
-      <q-input
-        filled
-        type="number"
-        v-model="age"
-        label="Your age *"
-        lazy-rules
-        :rules="[
-          (val) => (val !== null && val !== '') || 'Please type your age',
-          (val) => (val > 0 && val < 100) || 'Please type a real age',
-        ]"
-      />
-
-      <q-toggle v-model="accept" label="I accept the license and terms" />
-
-      <div>
-        <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn
-          label="Reset"
-          type="reset"
-          color="primary"
-          flat
-          class="q-ml-sm"
+    <div class="q-pa-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input
+          filled
+          v-model="name"
+          label="Longitude *"
+          hint="Example: 44.34"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
         />
-      </div>
-    </q-form>
+
+        <q-input
+          filled
+          v-model="age"
+          label="Latitude *"
+          hint="Example: 10.99"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" />
+          <q-btn
+            label="Reset"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
+    </div>
   </div>
 </template>
 
@@ -119,12 +79,11 @@ import { api } from "boot/axios";
 
 export default {
   setup() {
+    const visible = ref(false);
+    const showSimulatedReturnData = ref(false);
     const $q = useQuasar();
-
     const name = ref(null);
     const age = ref(null);
-    const accept = ref(false);
-
     const data = ref(null);
 
     function loadData() {
@@ -134,49 +93,57 @@ export default {
         )
         .then((response) => {
           data.value = response.data;
-          console.log(response.data);
-        })
-        .catch(() => {
-          $q.notify({
-            color: "negative",
-            position: "top",
-            message: "Loading failed",
-            icon: "report_problem",
-          });
+          console.log(data.value);
         });
     }
 
+    function onSubmit() {
+      api
+        .get(
+          "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=30292a6a5232adf186142c5e87134704"
+        )
+        .then((response) => {
+          data.value = response.data;
+          this.textFieldValue = "data.value.formattedString;";
+          console.log(data.value);
+        });
+
+      $q.notify({
+        color: "green-4",
+        textColor: "white",
+        icon: "cloud_done",
+        message: "Processing",
+      });
+
+      visible.value = true;
+      showSimulatedReturnData.value = false;
+
+      setTimeout(() => {
+        visible.value = false;
+        showSimulatedReturnData.value = true;
+      }, 3000);
+    }
+
     return {
+      visible,
+      showSimulatedReturnData,
+      onSubmit,
+      loadData,
       name,
       age,
-      accept,
       data,
-      loadData,
-
-      onSubmit() {
-        if (accept.value !== true) {
-          $q.notify({
-            color: "red-5",
-            textColor: "white",
-            icon: "warning",
-            message: "You need to accept the license and terms first",
-          });
-        } else {
-          $q.notify({
-            color: "green-4",
-            textColor: "white",
-            icon: "cloud_done",
-            message: "Submitted",
-          });
-        }
-      },
-
+      textFieldValue: "adasds",
       onReset() {
         name.value = null;
         age.value = null;
-        accept.value = false;
       },
     };
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.card-example
+  width: 288px
+  height: 290px
+</style>
